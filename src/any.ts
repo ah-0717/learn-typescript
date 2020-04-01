6e3ccaba6a3dca913dfa47c921cd7d9a8d38a3c7
ah-0717 <ah.recruit.1991@gmail.com>
2020-04-01 22:35:38 +0900

    create any.ts

---
+++ src/any.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+export {};
+
+const url: string =
+  'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
+
+axios.get(url).then((res: any) => {
+  interface Article {
+    id: number;
+    title: string;
+    description: string;
+  }
+
+  const data: Article[] = res.data;
+
+  console.log(data);
+});
