ea9c6450614b25b732f682b1b4912077cdc9de79
ah-0717 <ah.recruit.1991@gmail.com>
2020-04-04 23:05:03 +0900

    create literal.ts

---
+++ src/literal.ts
@@ -0,0 +1,13 @@
+export {};
+
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土';
+
+dayOfTheWeek = '水';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+month = 11;
+
+let TRUE: true;
+
+TRUE = true;
