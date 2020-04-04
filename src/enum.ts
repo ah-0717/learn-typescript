0bff6e3f9bb15b73ef19d50c7c5dbfc8eb69fd10
ah-0717 <ah.recruit.1991@gmail.com>
2020-04-04 23:17:03 +0900

    create enum.ts

---
+++ src/enum.ts
@@ -0,0 +1,32 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+
+Months.April;
+Months.November;
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#000000'
+}
+
+let red = COLORS.RED;
+
+enum COLORS {
+  YELLOW = '#FFFF00'
+}
+
+let yellow = COLORS.YELLOW;
