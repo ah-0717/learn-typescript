61a65e732fb83535d2026eb1da60a132105b7667
ah-0717 <ah.recruit.1991@gmail.com>
2020-04-05 23:59:39 +0900

    create my-first-class.ts

---
+++ src/my-first-class.ts
@@ -0,0 +1,15 @@
+export {};
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  profile(): string {
+    return `name: ${this.name}, age: ${this.age}`;
+  }
+}
+const taro = new Person('Taro', 30);
