7166f7779bd5a423b78566038fe42c784ecb9496
ah-0717 <ah.recruit.1991@gmail.com>
2020-04-07 23:47:40 +0900

    create namespaces.ts

---
+++ src/namespaces.ts
@@ -0,0 +1,28 @@
+export {};
+
+namespace Japanese {
+  export namespace Tokyo {
+    export class Parson {
+      constructor(public name: string) {}
+    }
+  }
+  export namespace Osaka {
+    export class Parson {
+      constructor(public name: string) {}
+    }
+  }
+}
+
+namespace English {
+  export class Parson {
+    constructor(
+      public firstName: string,
+      public middleName: string,
+      public lastName: string
+    ) {}
+  }
+}
+
+const me = new Japanese.Tokyo.Parson('aaa');
+
+const aaa = new English.Parson('a', 'a', 'a');
